6f1c4948fa9ccef5b35de3b3e90adaa2ef78b8b3
Debbie Welch <debbiewelchrealtor@gmail.com>
2026-08-26 02:53:07 -0700

    Enable static SEO rendering and performance improvements

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { createRoot } from "react-dom/client";
+import { createRoot, hydrateRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+const app = (
   <HelmetProvider>
     <App />
   </HelmetProvider>
 );
+
+const root = document.getElementById("root")!;
+
+if (root.hasChildNodes()) {
+  hydrateRoot(root, app);
+} else {
+  createRoot(root).render(app);
+}
+

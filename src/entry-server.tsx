03b7ef10dd1764b1914ce10c559974151e70014f
Debbie Welch <debbiewelchrealtor@gmail.com>
2026-08-26 02:53:13 -0700

    Enable static SEO rendering and performance improvements

---
+++ src/entry-server.tsx
@@ -0,0 +1,18 @@
+import { renderToString } from "react-dom/server";
+import { HelmetProvider, type HelmetServerState } from "react-helmet-async";
+import { StaticRouter } from "react-router-dom/server";
+import { AppShell } from "./App";
+
+export const render = (url: string) => {
+  const helmetContext: { helmet?: HelmetServerState } = {};
+  const html = renderToString(
+    <HelmetProvider context={helmetContext}>
+      <StaticRouter location={url}>
+        <AppShell />
+      </StaticRouter>
+    </HelmetProvider>
+  );
+
+  return { html, helmet: helmetContext.helmet };
+};
+
